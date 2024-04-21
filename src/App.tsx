d70eafd4bcbe7818cb85012ce50716ba78b3e156
Luiza Ferri <luizamanfrediniferri@hotmail.com>
2024-04-21 18:01:40 -0300

    ThemeToggle e menu da conta

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import { router } from "./routes";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { Toaster } from "sonner";
 import "@/global.css";
+import { ThemeProvider } from "./components/theme/theme-provider";
 
 function App() {
   return (
-    <HelmetProvider>
-      <Toaster  richColors/>
-      <Helmet titleTemplate="%s | pizza.shop" />
-      <RouterProvider router={router} />
-    </HelmetProvider>
+    <ThemeProvider storageKey="pizzashopp-theme" defaultTheme="dark">
+      <HelmetProvider>
+        <Toaster richColors />
+        <Helmet titleTemplate="%s | pizza.shop" />
+        <RouterProvider router={router} />
+      </HelmetProvider>
+    </ThemeProvider>
   );
 }
 
