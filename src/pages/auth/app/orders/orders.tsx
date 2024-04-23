025091cafb1b30dc87a99632a2cf881e7a04ac9f
Luiza Ferri <luizamanfrediniferri@hotmail.com>
2024-04-23 20:07:09 -0300

    Componente: Filtros de pedidos

--- src/pages/auth/app/orders/orders.tsx
+++ src/pages/auth/app/orders/orders.tsx
@@ -1,15 +1,13 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 import {
   Table,
   TableBody,
-  TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { ArrowRight, Search, X } from "lucide-react";
 import { Helmet } from "react-helmet-async";
+import { OrderTableRow } from "./orders-table-row";
+import { OrderTableFilter } from "./orders-table-filters";
 
 export function Orders() {
   return (
@@ -19,10 +17,7 @@ export function Orders() {
         <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
       </div>
       <div className="space-y-2.5">
-        <form className="flex items-center gap-2">
-          <span className="text-sm font-semibold">Filtros:</span>
-          <Input placeholder="Nome do cliente" className="h-8 w-[320px]" />
-        </form>
+        <OrderTableFilter />
         <div className="bolder rounded-md">
           <Table>
             <TableHeader>
@@ -38,43 +33,7 @@ export function Orders() {
               </TableRow>
             </TableHeader>
             <TableBody>
-              <TableRow>
-                <TableCell>
-                  <Button variant="outline" size="xs">
-                    <Search className="h-3 w-3" />
-                    <span className="sr-only">Detalhes do pedido</span>
-                  </Button>
-                </TableCell>
-                <TableCell className="font-mono text-xs font-medium">
-                  84845775758
-                </TableCell>
-                <TableCell className="text-muted-foreground">
-                  {" "}
-                  Há 15 minutos{" "}
-                </TableCell>
-                <TableCell>
-                  <div className="flex items-center gap-2">
-                    <span className="h-2 w-2 rounded-full bg-slate-400" />
-                    <span className="font-medium text-muted-foreground">
-                      Pendente
-                    </span>
-                  </div>
-                </TableCell>
-                <TableCell className="font-medium">Luiza Ferri</TableCell>
-                <TableCell className="font-medium">R$ 1.200,00</TableCell>
-                <TableCell>
-                  <Button variant="outline" size="xs">
-                    <ArrowRight className="h-3 w-3 mr-2" />
-                    Aprovar
-                  </Button>
-                </TableCell>
-                <TableCell>
-                  <Button variant="ghost" size="xs">
-                    <X className="h-3 w-3 mr-2" />
-                    Cancelar
-                  </Button>
-                </TableCell>
-              </TableRow>
+              <OrderTableRow />
             </TableBody>
           </Table>
         </div>
